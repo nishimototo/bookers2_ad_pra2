1f15f358c3c6ef1714f2f7e5e2db2785d53358b3
nishimototo <dkspm2436@gmail.com>
2021-07-12 05:33:38 +0000

    いいね実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
 
-  has_many :books, dependent: :destroy
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+ 
 end
