09eaed293e950eccecacd5850e8cf5af19698263
nishimototo <dkspm2436@gmail.com>
2021-07-16 07:25:58 +0000

    7a終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.includes(:favorites).sort{|a,b| b.favorites.size <=> a.favorites.size}
     @book = Book.new
     @user = current_user
   end
